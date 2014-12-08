b245e43df45873e260042707a029e8340c1a0e46
Jenny Yang <hyeayouy@andrew.cmu.edu>
2014-12-08 12:26:38 -0500

    Changed mongo configuration in concerts model

--- models/concerts.js
+++ models/concerts.js
@@ -1,15 +1,26 @@
 var util = require("util");
-var mongoClient = require('mongodb').MongoClient;
 /*
  * This is the connection URL
  * Give the IP Address / Domain Name (else localhost)
  * The typical mongodb port is 27012
  * ConcertTracker is the name of the database
  */
-var url = 'mongodb://127.0.0.1:27017/ConcertTracker';
+
+ // default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/footprints';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+var mongoClient = require('mongodb').MongoClient;
 var mongoDB; // The connected database
 // Use connect method to connect to the Server
-mongoClient.connect(url, function(err, db) {
+mongoClient.connect('mongodb://' + connection_string, function(err, db) {
   if (err) doError(err);
   console.log("Connected correctly to server");
   mongoDB = db;
